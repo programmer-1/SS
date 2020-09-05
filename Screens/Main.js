5677b0bfc3679d349cd45e71a07d195e471d9445
= <=>
2020-09-05 10:08:45 +0530

    responsive fonts

--- Screens/Main.js
+++ Screens/Main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View,Image,ImageBackground, Dimensions,Linking,Alert,TouchableWithoutFeedback, TouchableOpacity} from 'react-native';
-import { SocialIcon,Header} from 'react-native-elements'
+import { StyleSheet, Text, View,Image,ImageBackground,Dimensions, Platform, PixelRatio,Linking,Alert,TouchableWithoutFeedback, TouchableOpacity} from 'react-native';
+import { SocialIcon,Header, normalize} from 'react-native-elements'
 import Modal from 'react-native-modal';
 //import { Appbar } from 'react-native-paper';
 import {Video} from 'expo-av';
@@ -17,8 +17,14 @@ import KSYVideo from "react-native-ksyvideo";
 //import Video from 'react-native-video';
 //import playControls from 'react-native-vector-icons/FontAwesome5';
 
-
 const { width } = Dimensions.get('window');
+
+const {
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT,
+  } = Dimensions.get('window');
+
+const scale = SCREEN_WIDTH / 392;  
 
 export default class Main extends Component {
     constructor(){
@@ -42,6 +48,15 @@ export default class Main extends Component {
         };
         call(args).catch(console.error);
       };
+
+      normalize = (size) => {
+        const newSize = size * scale 
+        if (Platform.OS === 'ios') {
+          return Math.round(PixelRatio.roundToNearestPixel(newSize))
+        } else {
+          return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
+        }
+      }
 
       showAlert(phone,name) {  
         Alert.alert(  
@@ -91,9 +106,9 @@ Linking.openURL('https://goo.gl/maps/ZVhVyYTiEA5KcAMKA');
             <Header
                 backgroundColor = "#3668ff"
                 placement="left"
-                //leftComponent={<MyCustomLeftComponent />}
-                centerComponent={{ text: 'SS TV Ranipet', style: { color: '#fff',fontSize:22} }}
-                rightComponent={<Entypo name="info-with-circle" size={24} color="white" onPress = {() =>{this.setState({isinfo:true})}} />}
+                leftComponent={<Image  source={require('../Images/spla.png')} style={{width:normalize(70),height:normalize(35)}}/>}
+                centerComponent={{ text: 'SS TV Ranipet', style: { color: '#fff',fontSize:normalize(18)} }}
+                rightComponent={<Entypo name="info-with-circle" size={22} color="white" onPress = {() =>{this.setState({isinfo:true})}} />}
             />
                     {!this.state.isstream?<Image  source={require('../Images/Logo.png')} style={{width, height:width*.65}}/>:
                      <TouchableWithoutFeedback onPress = {this.timeout}>
@@ -136,7 +151,7 @@ Linking.openURL('https://goo.gl/maps/ZVhVyYTiEA5KcAMKA');
                         style={{width, height:width*.65}}
                         />
                         {/* <AntDesign name="closecircle" size={24} color="red" style = {{position:"absolute",top:10,right:5}}/> */}
-                        {this.state.isnavtoplayer?<FontAwesome name="external-link" size={24} color="white" style = {{position:"absolute",top:10,right:10}} onPress = {() =>{Linking.openURL('https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8')}}/>:null}
+                        {this.state.isnavtoplayer?<FontAwesome name="external-link" size={22} color="white" style = {{position:"absolute",top:10,right:10}} onPress = {() =>{Linking.openURL('https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8')}}/>:null}
                         </View>
                      </TouchableWithoutFeedback>
                     }
@@ -144,54 +159,54 @@ Linking.openURL('https://goo.gl/maps/ZVhVyYTiEA5KcAMKA');
                 <Modal animationIn="slideInUp" animationOut="slideOutDown" onBackdropPress={()=>this.setState({isinfo: false})}
                 
                 swipeDirection="right" isVisible={this.state.isinfo} style={{marginTop :"50%" ,backgroundColor:'white',maxHeight:Dimensions.get('window').height/3}}>
-                    <AntDesign name="closecircle" size={24} color="red" style = {{position:"absolute",top:10,right:5}} onPress={()=>this.setState({isinfo: false})}/>
+                    <AntDesign name="closecircle" size={normalize(24)} color="red" style = {{position:"absolute",top:10,right:5}} onPress={()=>this.setState({isinfo: false})}/>
                         <View style = {{alignItems:'center'}}>
-                        <Text style = {{fontSize:20,color:"#fc3d3d",fontWeight:"bold",}}>SS MEDIA NETWORK</Text>
-                        <Text style = {{fontSize:16,color:"#e770ff"}}>No:23, Nethaji street,Walajapet,Vellore</Text>
+                        <Text style = {{fontSize:normalize(18),color:"#fc3d3d",fontWeight:"bold",}}>SS TV WALAJA NETWORK</Text>
+                        <Text style = {{fontSize:normalize(13),color:"#e770ff"}}>No:23, Nethaji street,Walajapet,Vellore</Text>
                         </View>
                 </Modal>   
                 <View style = {styles.Bottom}>
                     <View style = {{flexDirection:"row",backgroundColor:"#fff",padding:20}}>
                         <View style = {{flex:1,alignItems:"center",justifyContent:"center"}}>
-                        <MaterialIcons name="live-tv" size={50} color="#292828" onPress = {()=>{this.setState(prevState =>({isstream:!prevState.isstream}))}}/>
+                        <MaterialIcons name="live-tv" size={normalize(45)} color="#292828" onPress = {()=>{this.setState(prevState =>({isstream:!prevState.isstream}))}}/>
                             <Text style = {styles.txtstylesocial}>On Air</Text>  
                         </View>
                         <View style = {{flex:1,alignItems:"center",justifyContent:"center"}}>
-                        <Entypo name="facebook-with-circle" size={50} color="#0080ff" onPress = {() =>{Linking.openURL('fb://page/105016241207647');}}/>
+                        <Entypo name="facebook-with-circle" size={normalize(45)} color="#0080ff" onPress = {() =>{Linking.openURL('fb://page/105016241207647');}}/>
                         <Text style = {styles.txtstylesocial}>Facebook</Text>  
                         </View>
                         <View style = {{flex:1,alignItems:"center",justifyContent:"center"}}>
-                        <MaterialCommunityIcons name="google-maps" size={50}  color="#37a670" onPress = {this.location}/>
+                        <MaterialCommunityIcons name="google-maps" size={normalize(45)}  color="#37a670" onPress = {this.location}/>
                         <Text style = {styles.txtstylesocial}>Location</Text>    
                         </View>
                         <View style = {{flex:1,alignItems:"center",justifyContent:"center"}}>
-                        <Ionicons name="md-call" size={50} color="#0080ff" onPress={this.showAlert.bind(this,'7339003295','SS MEDIA NETWORK')}/>
+                        <Ionicons name="md-call" size={45} color="#0080ff" onPress={this.showAlert.bind(this,'7339003295','SS MEDIA NETWORK')}/>
                             <Text style = {styles.txtstylesocial}>Call</Text>  
                         </View>
                     </View>
                     <View style = {{backgroundColor:"white",paddingVertical:20,width:"100%",justifyContent:"center",alignItems:"center",elevation:10,borderTopWidth:.5,borderTopColor:'#5c5c5c'}}>
-                        <Text style = {{fontSize:20,color:"#fc3d3d",fontWeight:"bold"}}>SS MEDIA NETWORK</Text>
-                        <Text style = {{fontSize:16,color:"#e770ff"}}>For Advertisements : 7339003295</Text>
-                        <Text style = {{fontSize:16,color:"#e770ff"}}>No:23, Nethaji street,Walajapet,Vellore</Text>
+                        <Text style = {{fontSize:normalize(16),color:"#fc3d3d",fontWeight:"bold"}}>SS TV WALAJA NETWORK</Text>
+                        <Text style = {{fontSize:normalize(13),color:"#e770ff"}}>For Advertisements : 7339003295</Text>
+                        <Text style = {{fontSize:normalize(13),color:"#e770ff"}}>No:23, Nethaji street,Walajapet,Vellore</Text>
                     </View>
-                    <View style = {{backgroundColor:"#4aa4ff",flexDirection:"row",paddingHorizontal:15,paddingVertical:20,width:"100%",justifyContent:"center",alignItems:"center"}}>
+                    <View style = {{backgroundColor:"#4aa4ff",flexDirection:"row",paddingHorizontal:10,paddingVertical:20,width:"100%",justifyContent:"center",alignItems:"center"}}>
                         <View style = {{borderRightColor:"black",borderRightWidth:1,paddingRight : 10}}>
                         <View style = {{flexDirection:"row",alignItems:"center"}} >
-                            <Ionicons name="md-call" size={13} color="white" style = {{paddingRight:5}}/>
+                            <Ionicons name="md-call" size={normalize(10)} color="white" style = {{paddingRight:5}}/>
                             <Text style = {styles.txtcall} onPress = {this.showAlert.bind(this,'9894530758','G Boobalan')}>G Boobalan   : 9894530758</Text>
                         </View>
                         <View style = {{flexDirection:"row",alignItems:"center"}} >
-                            <Ionicons name="md-call" size={13} color="white" style = {{paddingRight:5}}/>
+                            <Ionicons name="md-call" size={normalize(10)} color="white" style = {{paddingRight:5}}/>
                             <Text style = {styles.txtcall} onPress = {this.showAlert.bind(this,'9488438415','K Saravanan')}>K Saravanan : 9488438415</Text>
                         </View>
                         </View>
                         <View style = {{paddingLeft : 10}}>
                         <View style = {{flexDirection:"row",alignItems:"center"}} >
-                            <Ionicons name="md-mail" size={13} color="white" style = {{paddingRight:5}}/>
+                            <Ionicons name="md-mail" size={normalize(10)} color="white" style = {{paddingRight:5}}/>
                             <Text style = {styles.txtcall}>sstvwalaja@gmail.com</Text>
                         </View>
                         <View style = {{flexDirection:"row",alignItems:"center"}} >
-                            <Entypo name="network" size={13} color="white" style = {{paddingRight:5}}/>
+                            <Entypo name="network" size={normalize(10)} color="white" style = {{paddingRight:5}}/>
                             <Text style = {styles.txtcall}>www.sstvwalaja.in</Text>
                         </View>
                         </View>
@@ -216,7 +231,7 @@ const styles = StyleSheet.create({
         borderRightColor:'black',
         borderLeftWidth:1,
         borderRightWidth:1,
-        backgroundColor:"black"
+        backgroundColor:"black",
     },
     backgroundVideo: {
         width,
@@ -226,15 +241,15 @@ const styles = StyleSheet.create({
         alignItems:"center",
     },
     txtstyle:{
-        fontSize:14,
+        fontSize:normalize(14),
         color:"black",
     },
     txtcall:{
-        fontSize:13,
+        fontSize:normalize(10),
         color:"white",
     },
     txtstylesocial:{
-        fontSize:14,
+        fontSize:normalize(10),
         color:"black",
     }
   });
